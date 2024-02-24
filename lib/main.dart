587143b126152bfd07512c9a2a0fef36f6555710
dariawiencierz <wiencierz18@gmail.com>
2024-02-24 17:50:38 +0100

    Add get_it and injectable

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:trip_together/app/core/injection_container.dart';
 import 'app/app.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  configureDependencies();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
